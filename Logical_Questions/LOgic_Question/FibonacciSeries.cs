273f690a3f4545d213b5372879e6dac8138c8b81
Renuka <renu.bagave1901@gmail.com>
2024-06-11 18:14:43 +0530

    [Renuka]Add : Added program of fibonacci series

---
+++ Logical_Questions/LOgic_Question/FibonacciSeries.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOgic_Question
+{
+    internal class FibonacciSeries
+    {
+        public static int fibonacci(int n)
+        {
+            int first = 0;
+            int second = 1;
+            int result = 0;
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+            return result;
+        }
+        public static string FibbonacciSeries1(int n)
+        {
+            int a = 0, b = 1, c;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(a.ToString() + ",");
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return sb.ToString().Remove(sb.Length - 1); ;
+        }
+    }
+}
---
+++ Logical_Questions/LOgic_Question/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace LOgic_Question
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=============================================================");
+            Console.WriteLine("Enter number");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", FibonacciSeries.fibonacci(i));
+            }
+            Console.WriteLine(Environment.NewLine);
+
+            Console.Write("Fibbonacci Series of {0} is : {1} ", 10, FibonacciSeries.FibbonacciSeries1(n));
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("=============================================================");
+
+        }
+    }
+}
