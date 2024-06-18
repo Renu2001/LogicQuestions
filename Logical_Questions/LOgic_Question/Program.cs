c80553af707052ad94de5d683ee04e6cd99f2a25
Renuka <renu.bagave1901@gmail.com>
2024-06-18 17:44:24 +0530

    [Renuka]Added : Added swapping program

--- Logical_Questions/LOgic_Question/Program.cs
+++ Logical_Questions/LOgic_Question/Program.cs
@@ -7,18 +7,38 @@ namespace LOgic_Question
         
         static void Main(string[] args)
         {
+            //Console.WriteLine("=============================================================");
+            //Console.WriteLine("Enter number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("{0} ", FibonacciSeries.fibonacci(i));
+            //}
+            //Console.WriteLine(Environment.NewLine);
+
+            //Console.Write("Fibbonacci Series of {0} is : {1} ", 10, FibonacciSeries.FibbonacciSeries1(n));
+            //Console.WriteLine(Environment.NewLine);
+            //Console.WriteLine("=============================================================");
+
             Console.WriteLine("=============================================================");
             Console.WriteLine("Enter number");
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("{0} ", FibonacciSeries.fibonacci(i));
-            }
-            Console.WriteLine(Environment.NewLine);
-
-            Console.Write("Fibbonacci Series of {0} is : {1} ", 10, FibonacciSeries.FibbonacciSeries1(n));
-            Console.WriteLine(Environment.NewLine);
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Swapping with third variable");
+            Swap.SwapNumber(a, b);
+            Console.WriteLine("Swapping without third variable with / & *");
+            Swap.SwapNumber2(a,b);
+            Console.WriteLine("Swapping without third variable with + & -");
+            Swap.SwapNumber3(a, b);
+            Console.WriteLine("Swapping without third variable with ^");
+            Swap.SwapNumber4(a, b);
+            Console.WriteLine("=============================================================");
+            Console.WriteLine("Enter String");
+            string c = Console.ReadLine();
+            string d = Console.ReadLine();
+            Console.WriteLine("Swapping String");
+            Swap.SwapString(c, d);
             Console.WriteLine("=============================================================");
 
         }
---
+++ Logical_Questions/LOgic_Question/Swap.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOgic_Question
+{
+    internal class Swap
+    {
+        public static void SwapNumber(int a, int b)
+        {
+            Console.WriteLine("Before swapping"+" "+ a+", "+b);
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After swapping"+" " + a + ", " + b);
+        }
+
+        public static void SwapNumber2(int a, int b)
+        {
+            Console.WriteLine("Before swapping" + " " + a + ", " + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("After swapping" + " " + a + ", " + b);
+        }
+        public static void SwapNumber3(int a, int b)
+        {
+            Console.WriteLine("Before swapping" + " " + a + ", " + b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("After swapping" + " " + a + ", " + b);
+        }
+        public static void SwapNumber4(int a, int b)
+        {
+            Console.WriteLine("Before swapping" + " " + a + ", " + b);
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+            Console.WriteLine("After swapping" + " " + a + ", " + b);
+        }
+
+        public static void SwapString(string a,string b)
+        {
+            Console.WriteLine("Before swapping" + " " + a + ", " + b);
+            a = a + b;
+            b = a.Substring(0, a.Length - b.Length);
+            a = a.Substring(b.Length);
+            Console.WriteLine("After swapping" + " " + a + ", " + b);
+        }
+    }
+}
